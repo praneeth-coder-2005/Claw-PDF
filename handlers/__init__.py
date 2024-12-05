647c01a86103757ef4ad5dad1a835294e601b142
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-05 20:01:29 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,9 @@
+from .merge import register_handlers as register_merge
+from .compress import register_handlers as register_compress
+from .convert import register_handlers as register_convert
 
+# Combine all handlers into a single function
+def register_all_handlers(dp):
+    register_merge(dp)
+    register_compress(dp)
+    register_convert(dp)
