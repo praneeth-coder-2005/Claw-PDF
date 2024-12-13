e87242f2bc27d7783bcae3cee11de7a84f208b29
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-13 09:58:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+API_ID = 22250562  # Replace with your API ID
+API_HASH = "07754d3bdc27193318ae5f6e6c8016af"  # Replace with your API hash
+BOT_TOKEN = "7913483326:AAGWXALKIt9DJ_gemT8EpC5h_yKWUCzH37M"  # Replace with your bot token
